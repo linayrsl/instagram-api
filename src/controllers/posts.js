6548ef266e6b3a2d296d224755c1e3440ead60a5
Lina Yaroslavski <lina.yrsl@gmail.com>
2020-08-15 20:47:51 +0300

    Refactors posts queries

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -21,14 +21,66 @@ class Posts {
 
   async getPostById(req, res) {
     try {
-      const post = await Post
-        .findById(req.params.id)
-        .populate("user", ["avatar", "username"]);
-      if (!post) {
+      const currentUserId = req.user.id;
+      const posts = await Post
+        .aggregate()
+        .match({ _id: ObjectId(req.params.id) })
+        .sort({ createdAt: req.query.sort ? parseInt(req.query.sort, 10) : -1 })
+        .lookup({
+          from: "users", localField: "user", foreignField: "_id", as: "users",
+        })
+        .lookup({
+          from: "comments", localField: "_id", foreignField: "postId", as: "comments",
+        })
+        .addFields({
+          user: { $arrayElemAt: ["$users", 0] },
+          likesCount: {
+            $size: "$likes",
+          },
+          commentsCount: {
+            $size: "$comments",
+          },
+          isLikedByCurrentUser: {
+            // Searches for intersection between currentUserId and likes (in posts model) array.
+            // If current user liked the post
+            // then intersection returns array with 1 element
+            // and it's size is compared to 1 using $eq expression.
+            // The result of "$eq" expression is assigned to a field "isLikedByCurrentUser".
+            $eq: [
+              {
+                $size: {
+                  $ifNull: [
+                    {
+                      $setIntersection: [
+                        "$likes",
+                        [ObjectId(currentUserId)],
+                      ],
+                    },
+                    [],
+                  ],
+                },
+              },
+              1,
+            ],
+          },
+        })
+        .project({
+          _id: true,
+          createdAt: true,
+          description: true,
+          image: true,
+          likesCount: true,
+          commentsCount: true,
+          isLikedByCurrentUser: true,
+          "user._id": true,
+          "user.avatar": true,
+          "user.username": true,
+        });
+      if (posts.length === 0) {
         res.status(HTTP_STATUS_CODES.NOT_FOUND);
         return;
       }
-      res.status(HTTP_STATUS_CODES.OK).json(post);
+      res.status(HTTP_STATUS_CODES.OK).json(posts[0]);
     } catch (error) {
       console.log(error);
       res.status(HTTP_STATUS_CODES.BAD_REQUEST).json(error);
@@ -37,18 +89,73 @@ class Posts {
 
   async getPosts(req, res) {
     try {
+      // UserId query param will appear only when viewing posts of specific user
       const { userId } = req.query;
       const { page } = req.query;
       const POSTS_PER_PAGE = 10;
       const postsTOSkip = (page - 1) * POSTS_PER_PAGE;
-      const query = {};
+      // The signed in user
+      const currentUserId = req.user.id;
+      const match = {};
       if (userId) {
-        query.user = userId;
+        match.user = ObjectId(userId);
+      } else {
+        match.user = { $ne: ObjectId(currentUserId) };
       }
       const posts = await Post
-        .find(query)
-        .sort({ createdAt: req.query.sort || 1 })
-        .populate("user", ["_id", "avatar", "username"])
+        .aggregate()
+        .match(match)
+        .sort({ createdAt: req.query.sort ? parseInt(req.query.sort, 10) : -1 })
+        .lookup({
+          from: "users", localField: "user", foreignField: "_id", as: "users",
+        })
+        .lookup({
+          from: "comments", localField: "_id", foreignField: "postId", as: "comments",
+        })
+        .addFields({
+          user: { $arrayElemAt: ["$users", 0] },
+          likesCount: {
+            $size: "$likes",
+          },
+          commentsCount: {
+            $size: "$comments",
+          },
+          isLikedByCurrentUser: {
+            // Searches for intersection between currentUserId and likes (in posts model) array.
+            // If current user liked the post
+            // then intersection returns array with 1 element
+            // and it's size is compared to 1 using $eq expression.
+            // The result of "$eq" expression is assigned to a field "isLikedByCurrentUser".
+            $eq: [
+              {
+                $size: {
+                  $ifNull: [
+                    {
+                      $setIntersection: [
+                        "$likes",
+                        [ObjectId(currentUserId)],
+                      ],
+                    },
+                    [],
+                  ],
+                },
+              },
+              1,
+            ],
+          },
+        })
+        .project({
+          _id: true,
+          createdAt: true,
+          description: true,
+          image: true,
+          likesCount: true,
+          commentsCount: true,
+          isLikedByCurrentUser: true,
+          "user._id": true,
+          "user.avatar": true,
+          "user.username": true,
+        })
         .skip(postsTOSkip)
         .limit(POSTS_PER_PAGE);
       res.status(200).json(posts);
@@ -62,19 +169,15 @@ class Posts {
     try {
       const filter = { _id: ObjectId(req.params.id), likes: { $ne: ObjectId(req.user.id) } };
       const update = { $push: { likes: req.user.id } };
-      const post = await Post.findOneAndUpdate(
+      const result = await Post.updateOne(
         filter,
         update,
-        {
-          new: true,
-        },
-      )
-        .populate("user", ["_id", "avatar", "username"]);
-      if (post === null) {
+      );
+      if (result.n === 0) {
         res.sendStatus(HTTP_STATUS_CODES.CONFLICT);
         return;
       }
-      res.status(HTTP_STATUS_CODES.OK).json(post);
+      res.sendStatus(HTTP_STATUS_CODES.OK);
     } catch (error) {
       console.error(error);
       res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json(error);
@@ -85,16 +188,11 @@ class Posts {
     try {
       const filter = { _id: ObjectId(req.params.id) };
       const update = { $pull: { likes: ObjectId(req.user.id) } };
-      const post = await Post.findOneAndUpdate(
+      await Post.updateOne(
         filter,
         update,
-        {
-          new: true,
-        },
-      )
-        .populate("user", ["_id", "avatar", "username"]);
-      console.log(post);
-      res.status(HTTP_STATUS_CODES.OK).json(post);
+      );
+      res.sendStatus(HTTP_STATUS_CODES.OK);
     } catch (error) {
       console.error(error);
       res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json(error);
